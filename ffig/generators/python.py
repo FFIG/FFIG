d60312d30bf1b2fa43f4f59848301573b6c11006
Jonathan B. Coe <jbcoe@me.com>
2017-02-11 21:19:58 +0000

    use template to generate __init__.py

--- ffig/generators/python.py
+++ ffig/generators/python.py
@@ -9,16 +9,9 @@ def generator(module_name, binding, api_classes, env, output_dir):
     if not os.path.exists(module_dir):
         os.makedirs(module_dir)
 
-    with open(os.path.join(module_dir, '__init__.py'), 'w') as o:
-        o.write(
-"""import sys
-if sys.version_info[0] == 2:
-    from .interop_py2 import *
-elif sys.version_info[0] == 3:
-    from .interop_py3 import *
-else:
-    raise Exception('Unsupported Python version {0}'.format(sys.version_info[0]))
-""")
+    o = os.path.join(module_dir, '__init__.py')
+    generators.generate_single_output_file(module_name, '__init__.py.tmpl', api_classes, env, o)
+
     for o in [os.path.join(module_dir, x) for x in ['interop_py2.py', 'interop_py3.py']]:
         generators.generate_single_output_file(
             module_name, 'py.tmpl', api_classes, env, o)
