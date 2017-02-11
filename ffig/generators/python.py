076ed2b07257603aa0ba99faf4b3d9c720fb5fda
Jonathan B. Coe <jbcoe@me.com>
2017-02-11 21:02:31 +0000

    Improved python version check

--- ffig/generators/python.py
+++ ffig/generators/python.py
@@ -11,11 +11,13 @@ def generator(module_name, binding, api_classes, env, output_dir):
 
     with open(os.path.join(module_dir, '__init__.py'), 'w') as o:
         o.write(
-            """import sys
-if sys.version_info[0] == 3:  
+"""import sys
+if sys.version_info[0] == 2:
+    from .interop_py2 import *
+elif sys.version_info[0] == 3:
     from .interop_py3 import *
 else:
-    from .interop_py2 import *
+    raise Exception('Unsupported Python version {0}'.format(sys.version_info[0]))
 """)
     for o in [os.path.join(module_dir, x) for x in ['interop_py2.py', 'interop_py3.py']]:
         generators.generate_single_output_file(
