d6dcc073ad03dd015ef5f1d7c2ecee399671e476
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2017-02-11 16:59:20 +0000

    Fix import error.

--- ffig/generators/python/__init__.py
+++ ffig/generators/python/__init__.py
@@ -1,5 +1,5 @@
-from python2 import py2_generator
-from python3 import py3_generator
+from generators.python.python2 import py2_generator
+from generators.python.python3 import py3_generator
 
 def setup_plugin(context):
     context.register(py2_generator, ['py.tmpl'])
