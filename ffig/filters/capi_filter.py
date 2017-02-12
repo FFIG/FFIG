d09799369d7e9daa06d54aced01fa3ec9d034a60
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2017-02-12 14:23:07 +0000

    Basic Go bindings.
    This commit adds support for simple types in the Go bindings.
    It's possible to create an object and call simple functions with simple
    return types.
    
    There is no attempt to handle memory management / cleanup, so this can
    be assumed to leak resources as implemented.

--- ffig/filters/capi_filter.py
+++ ffig/filters/capi_filter.py
@@ -38,6 +38,29 @@ def to_c(t):
             return 'const void*'
     raise Exception('Type {} has no known c equivalent'.format(t.name))
 
+def to_go(t):
+    if t.kind == TypeKind.INT:
+        return 'int'
+    if t.kind == TypeKind.DOUBLE:
+        return 'float64'
+    if t.kind == TypeKind.POINTER:
+        if t.pointee.kind == TypeKind.CHAR_S:
+            return 'string'
+        if t.pointee.kind == TypeKind.RECORD:
+            return 'unsafe.Pointer'
+    raise Exception('Type {} has no known Go equivalent'.format(t.name))
+
+def to_go_convert(t):
+    if t.kind == TypeKind.INT:
+        return 'int'
+    if t.kind == TypeKind.DOUBLE:
+        return 'float64'
+    if t.kind == TypeKind.POINTER:
+        if t.pointee.kind == TypeKind.CHAR_S:
+            return 'C.GoString'
+        if t.pointee.kind == TypeKind.RECORD:
+            return 'unsafe.Pointer'
+    raise Exception('Type {} has no known Go equivalent'.format(t.name))
 
 #C++ header filter to extract C type from C++ type
 def c_object(v,t):
