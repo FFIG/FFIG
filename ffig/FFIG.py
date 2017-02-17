21f61e548eaeaed8f7984274448abb72d3855d8e
petr-tik <petr-tik@users.noreply.github.com>
2017-02-17 00:06:36 +0000

    Corrected method signature and changed comments
    Will need to call this method from rest-api, realised I was passing
    wrong args to it

--- ffig/FFIG.py
+++ ffig/FFIG.py
@@ -66,15 +66,16 @@ def get_class_name(header_path):
     return re.sub(".h$", "", header_name)
 
 
-def write_bindings_to_disk(module_name, api_classes, env, output_dir):
+def write_bindings_to_disk(args, api_classes, env, output_dir):
     """
     Write the bindings to disk, return Nothing
     Input:
+    - args from command line
     - api_classes
     - environment to get templates from
-    - args
     - output_dir where to write to
     """
+    module_name = args.module_name
     for binding in args.bindings:
         generators.generate(module_name, binding, api_classes, env, output_dir)
 
@@ -119,7 +120,7 @@ def main(args):
         # for f in ['to_output_ctype', 'to_ctype']:
         env.filters[f] = getattr(filters.capi_filter, f)
 
-    write_bindings_to_disk(args.module_name, api_classes, env, output_dir)
+    write_bindings_to_disk(args, api_classes, env, output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
