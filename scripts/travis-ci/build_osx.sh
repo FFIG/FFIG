03b7e407a009fc60f6152286073a198b3d69d12e
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2017-05-31 23:34:31 +0100

    N209: Move all commands back into .travis.yml

--- scripts/travis-ci/build_osx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Travis CI `script` phase for macOS.
-# Run ffig build.
-
-./scripts/build.py -t
-./scripts/build.py -t --python-path python3
--- scripts/travis-ci/install_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Travis CI `install` phase for Linux.
-# Build a docker image. The base image contains all of the dependencies.
-
-docker pull ffig/ffig-base
-docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} .
