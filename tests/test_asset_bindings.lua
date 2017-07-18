8f74ea3b2fbe567711605e5fa39a2406b02bcc43
Jonathan B. Coe <jbcoe@me.com>
2017-07-18 19:22:53 +0100

    Call C-function from Lua

--- tests/test_asset_bindings.lua
+++ tests/test_asset_bindings.lua
@@ -1,5 +1,7 @@
-require("../build/generated/Asset")
+require("Asset")
 
 a = Asset:new()
 assert(a.name()=="I'm afraid I can't do that")
 assert(a.value()=="I'm afraid I can't do that")
+
+assert(Asset_error() == "")
