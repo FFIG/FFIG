fcbf5e35c68094f6afe830cf2b5356d0916df0d1
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2017-08-20 18:43:17 +0100

    N257: Fix code formatting.

--- scripts/install-git-hooks.py
+++ scripts/install-git-hooks.py
@@ -13,8 +13,9 @@ for hook_script in hook_scripts:
     target = os.path.join(this_dir, hook_script)
     link_name = os.path.join(git_dir, 'hooks', hook_script.replace('.py', ''))
     if os.path.exists(link_name):
-        print('Skipping {0} because {1} already exists'.format(target, link_name))
+        print(
+            'Skipping {0} because {1} already exists'.format(
+                target, link_name))
     else:
         print('Installing symbolic link {0} --> {1}'.format(target, link_name))
         os.symlink(target, link_name)
-
