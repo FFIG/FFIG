66d29b33fb619087e9eebc71930e77c1e039462e
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2017-08-20 16:48:14 +0100

    N266: Code formatting fixes.
    Closes #266.

--- ffig/generators/generator_aliases.py
+++ ffig/generators/generator_aliases.py
@@ -4,18 +4,20 @@
 import ffig.generators
 
 aliases = {
-        'lua': 'lua.tmpl',
-        'ruby': 'rb.tmpl',
-        }
+    'lua': 'lua.tmpl',
+    'ruby': 'rb.tmpl',
+}
+
 
 def aliased_generator(module_name, binding, api_classes, env, output_dir):
     try:
         binding = aliases[binding]
     except KeyError:
         raise Exception('No alias for {0}'.format(binding))
-    
-    return ffig.generators.default_generator(module_name, binding, api_classes, env, output_dir)
+
+    return ffig.generators.default_generator(
+        module_name, binding, api_classes, env, output_dir)
+
 
 def setup_plugin(context):
     context.register(aliased_generator, aliases.keys())
-
