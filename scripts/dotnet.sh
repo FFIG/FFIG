0fa585c8688dd67f4ff952838becaec8f503461e
Jonathan B. Coe <jonathanbcoe@gmail.com>
2017-10-30 10:13:53 +0000

    shared library extension removed from dotnet bindings - let it be deduced

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -9,5 +9,5 @@ gen_dir=${2:-`pwd`/build/generated}
 cp ${src_dir}/tests/dotnet/*.cs ${gen_dir}
 cp ${src_dir}/tests/dotnet/ffig.net.csproj ${gen_dir}
 
-(cd ${gen_dir}; dotnet restore; LD_LIBRARY_PATH=${gen_dir} dotnet test)
+(cd ${gen_dir}; dotnet restore; LD_LIBRARY_PATH=${gen_dir} dotnet test -o .)
 
