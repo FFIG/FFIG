5a744fdc59c5dc6eeaf010524bfec0658ee9f069
Jonathan B. Coe <jonathanbcoe@gmail.com>
2017-10-30 20:11:26 +0000

    get CMake to generate dotnet test files

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -3,11 +3,7 @@
 set -e
 set -u
 
-src_dir=${1:-`pwd`}
-gen_dir=${2:-`pwd`/build/generated}
-
-cp ${src_dir}/tests/dotnet/*.cs ${gen_dir}
-cp ${src_dir}/tests/dotnet/ffig.net.csproj ${gen_dir}
+gen_dir=${1:-`pwd`/build/generated}
 
 (cd ${gen_dir}; dotnet restore; LD_LIBRARY_PATH=${gen_dir} dotnet test -o .)
 
