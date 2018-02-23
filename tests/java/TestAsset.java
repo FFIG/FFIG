c3c18fddcb94ab5c2e5cada87efea941341bee3c
Jonathan B. Coe <jonathanbcoe@gmail.com>
2018-02-23 21:18:31 +0000

    Script to download JARs works

--- tests/java/TestAsset.java
+++ tests/java/TestAsset.java
@@ -7,7 +7,15 @@ public class TestAsset {
     public void CDOhasPVofZero()
     {
         Asset cdo = Asset.Asset_CDO_create();
-        double pv = cdo.value();
-        assertEquals(99.99, pv, 0.0);
+
+        assertEquals(99.99, cdo.value(), 0.0);
+    }
+
+    @Test
+    public void CDOisCalledCDO()
+    {
+        Asset cdo = Asset.Asset_CDO_create();
+
+        assertEquals(cdo.name(), "CDO");
     }
 }
