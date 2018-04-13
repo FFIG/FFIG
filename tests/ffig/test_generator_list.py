ffdd2b047677c1832067f047e705edb5271de40b
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2018-04-13 23:40:16 +0100

    N402: Fix test by converting filter(...) output to a list.

--- tests/ffig/test_generator_list.py
+++ tests/ffig/test_generator_list.py
@@ -2,7 +2,7 @@ import ffig.generators
 from nose.tools import assert_equals
 
 def test_generator_list():
-    python_entry = filter(lambda x: x[0] == 'python',
-           ffig.generators.generator_context.list_generators())[0]
+    python_entry = list(filter(lambda x: x[0] == 'python',
+        ffig.generators.generator_context.list_generators()))[0]
 
     assert_equals(python_entry[1], 'Python2 and Python3 generator using ctypes')
