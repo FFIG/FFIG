65ed8cf67a355207c273ceb55ee39f4ac37299f1
Jonathan B. Coe <jonathanbcoe@gmail.com>
2018-04-18 09:32:52 +0100

    Add swift bridging header generation and textual content test

--- ffig/generators/swift.py
+++ ffig/generators/swift.py
@@ -5,11 +5,19 @@ import os
 
 
 def generator(module_name, binding, api_classes, env, output_dir):
+    outputs = []
+
     o = os.path.join(output_dir, module_name + '.swift')
     ffig.generators.generate_single_output_file(
         module_name, 'swift.tmpl', api_classes, env, o)
+    outputs.append(o)
 
-    return [o]
+    o = os.path.join(output_dir, module_name + '-Bridging-Header.h')
+    ffig.generators.generate_single_output_file(
+        module_name, 'swift.bridging-header.tmpl', api_classes, env, o)
+    outputs.append(o)
+    
+    return outputs
 
 
 def setup_plugin(context):
